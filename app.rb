3df4b63c85b4a6c583c89e209487c94b6a46ff30
Ling Jiang <kyooryoo@hotmail.com>
2018-05-30 23:26:49 +0800

    second commit

--- app.rb
+++ app.rb
@@ -1,70 +1,40 @@
-# web server and flash messages
 require 'sinatra'
-
-# load environment variables
-# from .env file
 require 'dotenv'
+require 'nexmo'
+
 Dotenv.load
 
-# specify a layout file
-set :erb, layout: :layout
+set :erb, 
+layout: :layout
 
-# nexmo library
-require 'nexmo'
-nexmo = Nexmo::Client.new(
-  key: ENV['NEXMO_API_KEY'],
-  secret: ENV['NEXMO_API_SECRET']
-)
-
-# Index
-# - collects a phone number
-#
 get '/' do
   erb :index
 end
 
-# Notify
-# - Send the user their delivery
-#   notification, asking them
-#   to respond back if they
-#   want to make any changes
-#
-post '/notify' do
-  notification = "Your delivery is scheduled for tomorrow between " +
-                 "8am and 2pm. If you wish to change the delivery date please " +
-                 "reply by typing 1 (tomorrow), 2 (Thursday) or 3 (deliver to"
-                 "post office) below.\n\n";
-
-  nexmo.send_message(
-    from: ENV['NEXMO_NUMBER'],
-    to: params['number'],
-    text: notification
+post '/' do  
+  $nexmo = Nexmo::Client.new(
+    key: params['api_key'], #ENV['NEXMO_API_KEY'],
+    secret: params['api_secret'] #ENV['NEXMO_API_SECRET']
   )
-
-  "Notification sent to #{params['number']}"
+  erb :send
 end
 
-# Receive incoming message
-#
-# - Receives incoming SMS
-#   message, stores it, and
-#   notifies sender
-#
-get '/update' do
-  choice = params['text']
-  number = params['msisdn']
+get '/send' do
+  erb :send
+end
 
-  # You can store or validate
-  # the choice made here
+post '/send' do
+  @numbers = params['number']
+  @number_list = @numbers.split("\r\n")
 
-  message = "Thank you for picking option #{choice}. " +
-            "Your delivery is now fully scheduled in."
-
-  nexmo.send_message(
-    from: ENV['NEXMO_NUMBER'],
-    to: number,
-    text: message
-  )
-
-  body ''
+  for @number in @number_list
+    @number = params['prefix'] + @number
+    $nexmo.sms.send(
+      from: 'NEXMO',
+      to: @number,
+      text: params['message']
+    )
+    sleep 0.03
+  end
+  erb :send
 end
